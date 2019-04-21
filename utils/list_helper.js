cb89d097db3c3d78cc67521fbb39e2988d7f846c
Aija Valleala <aija.valleala@aalto.fi>
2019-04-21 10:22:46 +0300

    tehtävä 4.5

---
+++ utils/list_helper.js
@@ -0,0 +1,19 @@
+const dummy = blogs => {
+  return 1;
+};
+
+const totalLikes = blogs => {
+  const reducer = (sum, item) => {
+    return sum + item;
+  };
+  return blogs.map(b => b.likes).reduce(reducer, 0);
+};
+
+const favoriteBlog = blogs => {
+  const reducer = (acc, curr) => {
+    return acc.likes > curr.likes ? acc : curr;
+  };
+  return blogs.length === 0 ? undefined : blogs.reduce(reducer, 0);
+};
+
+module.exports = { dummy, totalLikes, favoriteBlog };
