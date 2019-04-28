ad1fee268092e498f24093a832c543ebbf67a834
Aija Valleala <aija.valleala@aalto.fi>
2019-04-28 18:08:27 +0300

    tehtävä 4.18

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const middleware = require("./utils/middleware");
 const mongoose = require("mongoose");
 const blogRouter = require("./controllers/blogs");
 const userRouter = require("./controllers/users");
+const loginRouter = require("./controllers/login");
+
 mongoose.set("useFindAndModify", false);
 mongoose.set("useCreateIndex", true);
 
@@ -18,6 +20,7 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use("/api/blogs", blogRouter);
 app.use("/api/users", userRouter);
+app.use("/api/login", loginRouter);
 app.use(middleware.errorHandler);
 
 module.exports = app;
