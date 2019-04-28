01facdf4146e555dea2c005d23590a6c3913a36e
Aija Valleala <aija.valleala@aalto.fi>
2019-04-28 11:21:51 +0300

    käyttäjän lisääminen aloitus

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ const cors = require("cors");
 const middleware = require("./utils/middleware");
 const mongoose = require("mongoose");
 const blogRouter = require("./controllers/blogs");
+const userRouter = require("./controllers/users");
 mongoose.set("useFindAndModify", false);
+mongoose.set("useCreateIndex", true);
 
 console.log("connecting to", config.MONGODB_URI);
 
@@ -15,6 +17,7 @@ mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true });
 app.use(cors());
 app.use(bodyParser.json());
 app.use("/api/blogs", blogRouter);
+app.use("/api/users", userRouter);
 app.use(middleware.errorHandler);
 
 module.exports = app;
